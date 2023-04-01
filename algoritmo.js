aac00409a5f2bdcf7a78060575833321a31ae12e
Antu <122485403+antukung@users.noreply.github.com>
2023-03-31 22:44:35 -0300

    primer y unico comit
    espero que esto lo recuerden los grandes

---
+++ algoritmo.js
@@ -0,0 +1,7 @@
+let entero =prompt ("escribe un numero!");
+
+    if (entero > 0 & entero < 100) {
+        alert (" es numero positivo y menor a 100");
+    } else {
+        alert ("no es numero positivo o no es menor a 100 ");
+    }
